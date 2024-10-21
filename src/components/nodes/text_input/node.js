5c2d713c0d45451c568d27dd33dceaa362f6dda1
airzostorm <airzostorm@outlook.com>
2024-10-22 00:19:18 +0800

    添加文本输入节点，整理节点样式

---
+++ src/components/nodes/text_input/node.js
@@ -0,0 +1,24 @@
+import NodeVue from './node.vue';
+export const initInfo = {
+    name: "文本输入",
+    type: "text_input_node",
+    init_width: 100,
+    init_height: 50,
+    init_data: {
+        // 必要参数 ========
+        _is_nested: false,// 是否可嵌套
+        _is_attached: false,// 是否为附属节点
+        _size: {
+            width: -1,
+            height: -1,
+        },
+        input:{},
+        callback:{},
+        output:{},
+        // =================
+        // 可选参数
+        // =================
+    },
+};
+
+export { NodeVue };
---
+++ src/components/nodes/text_input/node.vue
@@ -0,0 +1,33 @@
+<template>
+    <div :style="{ width: '100%', height: '100%' }">
+        <Handle id="input" type="target" :position="Position.Left" :style="{ left: '3px' }" />
+        <Handle id="output" type="source" :position="Position.Right" :style="{ right: '3px' }" />
+        <Handle id="callback-user" type="target" :position="Position.Top" :style="{ top: '3px' }" />
+        <Handle id="callback-provider" type="source" :position="Position.Bottom" :style="{ bottom: '3px' }" />
+        <n-flex vertical class="center-flex" justify="center" align="center">
+            <div class="center-flex-div">文本输入</div>
+        </n-flex>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount, onUnmounted, watch } from 'vue';
+import { Position, Handle } from '@vue-flow/core'
+import { NTag, NText, NH3, NFlex } from 'naive-ui';
+const props = defineProps(['id', 'label', 'data'])
+
+</script>
+
+<style scoped>
+.center-flex {
+    width: 100%;
+    height: 100%;
+}
+
+.center-flex-div {
+    color: white;
+    height: 20px;
+    display: flex;
+    align-items: center;
+}
+</style>
