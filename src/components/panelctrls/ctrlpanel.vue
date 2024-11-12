dc1108be55820957f2f357278da6fa0b4861a25f
zhuowenhao <zhuowenhao@huya.com>
2024-11-12 18:21:00 +0800

    添加临时的保存载入功能

--- src/components/panelctrls/ctrlpanel.vue
+++ src/components/panelctrls/ctrlpanel.vue
@@ -10,17 +10,32 @@ import {
     NFlex,
 } from 'naive-ui';
 import { Panel, useVueFlow } from '@vue-flow/core'
-
+// import { onSave, onRestore } from '../../utils/tools.js'
 const message = useMessage();
 
 const testclick = () => {
     message.success('test')
 }
+const { toObject, fromObject } = useVueFlow()
+
+function onSave(flowKey) {
+    localStorage.setItem(flowKey, JSON.stringify(toObject()));
+}
+
+function onRestore(flowKey) {
+    const flow = JSON.parse(localStorage.getItem(flowKey));
+
+    if (flow) {
+        fromObject(flow);
+    }
+}
 </script>
 
 <template>
     <n-flex justify="flex-end">
-        <n-button class="glow-btn" strong tertiary round type="success" @click="testclick">保存</n-button>
+        <n-button class="glow-btn" strong tertiary round type="success" @click="onSave('vueflow-store')">保存</n-button>
+        <n-button class="glow-btn" strong tertiary round type="success"
+            @click="onRestore('vueflow-store')">载入</n-button>
         <n-button class="glow-btn" strong tertiary round type="success" @click="testclick">运行</n-button>
         <n-button class="glow-btn" strong tertiary round type="success" @click="testclick">导入</n-button>
         <n-button class="glow-btn" strong tertiary round type="success" @click="testclick">导出</n-button>
