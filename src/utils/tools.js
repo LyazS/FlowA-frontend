3ff3cf5ac3a128f5b7b4fd502e73f63dce117370
airzostorm <airzostorm@outlook.com>
2024-11-26 22:54:15 +0800

    添加sse工具

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -69,3 +69,56 @@ export const isPathConnected = (obj, path) => {
         return false;
     }
 };
+
+export function SubscribeSSE(
+        url,
+        method,
+        headers,
+        body,
+        onOpen,
+        onMessage,
+        onClose,
+        onError,
+    ) {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    async function subscribe() {
+        try {
+            let sseconfig = {
+                method: method,
+                signal: controller.signal,
+                ...(headers !== null && { headers }),
+                ...(body !== null && { body }),
+                async onopen(event) {
+                    if (signal.aborted) {
+                        return;
+                    };
+                    await onOpen(event);
+                },
+                async onmessage(event) {
+                    if (signal.aborted) {
+                        return;
+                    };
+                    await onMessage(event);
+                },
+                async onclose() { await onClose(); controller.abort(); },
+                async onerror(err) { await onError(err); controller.abort(); throw err; },
+            }
+            console.log('Subscribing to SSE:', url);
+            await fetchEventSource(url, sseconfig);
+        } catch (err) {
+            console.error('fetchEventSource error:', err);
+            await onError(err);
+            controller.abort();
+        }
+    }
+
+    function unsubscribe() {
+        controller.abort();
+        console.log('SSE subscription unsubscribed.');
+    }
+    return {
+        subscribe,
+        unsubscribe,
+    }
+}
