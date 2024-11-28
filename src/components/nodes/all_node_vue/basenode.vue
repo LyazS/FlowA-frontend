4849ff899e4fd270127b49789fe551d7e7ba4641
zhuowenhao <zhuowenhao@huya.com>
2024-11-28 10:19:11 +0800

    清理代码

--- src/components/nodes/all_node_vue/basenode.vue
+++ src/components/nodes/all_node_vue/basenode.vue
@@ -46,7 +46,7 @@
             </div>
         </template>
 
-        <div class="center-text" @click="testclick"
+        <div class="center-text"
             :style="{ top: `${center_text_pos.top}px`, transform: `translate(-50%, ${center_text_pos.trfY}%)` }">
             {{ data.label }}
         </div>
@@ -167,26 +167,7 @@ onMounted(() => {
     }
     else { }
 });
-// const testclick = () => {
-//     if (thisnode.data.state.status === 'Default') {
-//         thisnode.data.state.status = 'Pending';
-//     }
-//     else if (thisnode.data.state.status === 'Pending') {
-//         thisnode.data.state.status = 'Running';
-//     }
-//     else if (thisnode.data.state.status === 'Running') {
-//         thisnode.data.state.status = 'Success';
-//     }
-//     else if (thisnode.data.state.status === 'Success') {
-//         thisnode.data.state.status = 'Canceled';
-//     }
-//     else if (thisnode.data.state.status === 'Canceled') {
-//         thisnode.data.state.status = 'Error';
-//     }
-//     else if (thisnode.data.state.status === 'Error') {
-//         thisnode.data.state.status = 'Default';
-//     }
-// }
+
 </script>
 
 <style>
@@ -205,8 +186,7 @@ onMounted(() => {
             #546e7a,
             #78909c,
             #546e7a,
-            #2c3e50
-        );
+            #2c3e50);
     background-size: 300% 300%;
     animation: emeraldWave 8s ease infinite;
 }
