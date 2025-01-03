697a137a8a0eac6c91f28868523f6fa85ca06927
airzostorm <airzostorm@outlook.com>
2025-01-04 00:00:56 +0800

    feat(nodes): rename and update template to jinja2_template

--- src/components/nodes/all_node_js/jinja2_template.js
+++ src/components/nodes/all_node_js/jinja2_template.js
@@ -25,14 +25,15 @@ import {
 
 import { cloneDeep } from 'lodash';
 import NodeVue from '../all_node_vue/basenode.vue';
+import { watch } from 'vue';
 
 const _initInfo = createBaseNodeInfo();
 initConnectionsAttribute(_initInfo);
 initRunningAttribute(_initInfo);
 initStateAttribute(_initInfo);
-setNodeType(_initInfo, "template_convertor");
+setNodeType(_initInfo, "jinja2_template");
 setVueType(_initInfo, "basenode");
-setLabel(_initInfo, "模板转换");
+setLabel(_initInfo, "Jinja2模板");
 initSize(_initInfo, 80, 80);
 
 addHandle(_initInfo, "inputs", "input");
@@ -53,6 +54,21 @@ addPayload(_initInfo, {
     uitype: "codeeditor", config: { language: "django" }
 }, 'D_CODE');
 
-export const initInfo = cloneDeep(_initInfo);
+addResult(_initInfo, {
+    label: "输入变量", type: "Dict", key: "inputvars", data: {
+    }
+}, "D_OUTPUT");
 
+export const initInfo = cloneDeep(_initInfo);
+export const initFunc = (node) => {
+    watch(
+        () => node.data.payloads.byId["D_VARSINPUT"].data,
+        (newVal) => {
+            node.data.results.byId["D_OUTPUT"].data = {};
+            for (let i = 0; i < newVal.length; i++) {
+                node.data.results.byId["D_OUTPUT"].data[newVal[i].key] = newVal[i].value;
+            }
+        }
+    )
+}
 export { NodeVue };
